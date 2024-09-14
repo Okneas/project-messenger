4ea08c4185b2848e4a34f4892ca196738b450532
TribalFcknChief <alstoykovana@gmail.com>
2024-09-14 16:24:45 +0300

    added chats and contacts pages

---
+++ src/pages/ContactsPage/components/Footer.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Typography } from "@mui/material";
+import { FooterWrapper } from "../style";
+import { useLocation } from "react-router-dom";
+import { GroupIcon } from "../../../svg/GroupIcon";
+import { CommunicationIcon } from "../../../svg/CommunicationIcon";
+import { OthersIcon } from "../../../svg/OthersIcon";
+import { EllipseIcon } from "../../../svg/EllipseIcon";
+
+export default function Footer(){
+    const location = useLocation();
+    if(location.pathname === '/'){
+        return null;    
+    }
+    return(
+        <Box component="footer">
+            <FooterWrapper sx={{display:"flex", flexDirection:"row", justifyContent:"space-around"}}>
+                {location.pathname === '/contacts' ? <Button><Box><Typography sx={{textTransform:"initial", color:"#0F1828",fontWeight:"Medium" }}>Контакты</Typography><EllipseIcon></EllipseIcon></Box></Button>: <Button><GroupIcon></GroupIcon></Button> }
+                {location.pathname === '/contacts' ? <Button><CommunicationIcon></CommunicationIcon></Button>: <Button><Box><Typography sx={{textTransform:"initial", color:"#0F1828",fontWeight:"Medium" }}>Чаты</Typography><EllipseIcon></EllipseIcon></Box></Button>}
+                {location.pathname === '/contacts' ? <Button><OthersIcon></OthersIcon></Button>: <Button><OthersIcon></OthersIcon></Button>}
+            </FooterWrapper>
+        </Box>
+    )
+}
